57ddbaab3b0ab0f5a77ea308379f78922bb8cb5a
邵謙(Shau Chien) <cvcvbnbn13@gmail.com>
2020-10-19 21:35:43 +0800

    new-world2

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const scv = document.createElement('div');
 const scx = document.createElement('h3');
-const scp = document.createElement('p');
+const scp = document.createElement('textarea');
 ooww
 'hello world';
